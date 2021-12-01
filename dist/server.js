58f8580945cf3697ed8a8f9bfbbabf4dfc87de99
Hanna <sophianhanna@gmail.com>
2021-12-01 16:55:31 +0000

    Idk

---
+++ dist/server.js
@@ -0,0 +1,64 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const pg_1 = require("pg");
+const client = new pg_1.Client({ database: 'yogadb' });
+//TODO: this request for a connection will not necessarily complete before the first HTTP request is made!
+client.connect();
+const app = (0, express_1.default)();
+// Connect to front-end
+app.use((0, cors_1.default)());
+/**
+ * Middleware to parse a JSON body in requests
+ */
+app.use(express_1.default.json());
+//When this route is called, return the most recent 100 signatures in the db
+app.get("/videos", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const videos = yield client.query('SELECT * FROM vids;'); //FIXME-TASK: get signatures from db!
+    res.status(200).json({
+        status: "success",
+        data: {
+            videos
+        },
+    });
+}));
+//get random video when input level, duration, tag
+app.get("/getvideo/:level/:duration/:tag", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const text = "SELECT * FROM vids JOIN vidtags ON vids.id = vidtags.id WHERE vidtags.tag = $3 AND vids.level <= $1 AND vids.duration <= $2 ORDER BY RANDOM() LIMIT 1;";
+        const video = yield client.query(text, [req.params.level, req.params.duration, req.params.tag]);
+        if (video.rowCount != 0) {
+            res.status(200).json({
+                status: "success",
+                data: {
+                    video
+                },
+            });
+        }
+        else {
+            res.status(404).json({
+                status: "fail",
+                data: {
+                    id: "Could not find a video with those arguments.",
+                },
+            });
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}));
+exports.default = app;
--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,7 @@ client.connect();
 
 
 const app = express();
-
-/**
- * Simplest way to connect a front-end. Unimportant detail right now, although you can read more: https://flaviocopes.com/express-cors/
- */
+// Connect to front-end
 app.use(cors());
 
 /**
@@ -32,109 +29,36 @@ app.get("/videos", async (req, res) => {
   });
 });
 
-// app.get("/signatures/:id", async (req, res) => {
-//   // :id indicates a "route parameter", available as req.params.id
-//   //  see documentation: https://expressjs.com/en/guide/routing.html
-//   const id = parseInt(req.params.id); // params are always string type
 
-//   const signature = await client.query('SELECT * from signatures WHERE id = $1;', [id]);   //FIXME-TASK get the signature row from the db (match on id)
+//get random video when input level, duration, tag
+app.get<{ level: number, duration: string, tag: string }>("/getvideo/:level/:duration/:tag", async (req, res) => {
+  try {
+    const text =
+      "SELECT * FROM vids JOIN vidtags ON vids.id = vidtags.id WHERE vidtags.tag = $3 AND vids.level <= $1 AND vids.duration <= $2 ORDER BY RANDOM() LIMIT 1;";
 
-//   if (signature.rowCount != 0) {
-//     res.status(200).json({
-//       status: "success",
-//       data: {
-//         signature,
-//       },
-//     });
-//   } else {
-//     res.status(404).json({
-//       status: "fail",
-//       data: {
-//         id: "Could not find a signature with that id identifier",
-//       },
-//     });
-//   }
-// });
+    const video = await client.query(text, [req.params.level, req.params.duration, req.params.tag]);
 
-// app.post("/signatures", async (req, res) => {
-//   const { name, message } = req.body;
-//   if (typeof name === "string") {
-//     const createdSignature = await client.query('INSERT INTO signatures (signature, message) VALUES ($1, $2) RETURNING *;', [name, message]); //FIXME-TASK: insert the supplied signature object into the DB
+    if (video.rowCount != 0) {
+      res.status(200).json({
+        status: "success",
+        data: {
+          video
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+        data: {
+          id: "Could not find a video with those arguments.",
+        },
+      });
+    }
+    
+  } catch(err) {
+    console.log(err)
+  } 
+})
 
-//     res.status(201).json({
-//       status: "success",
-//       data: {
-//         signature: createdSignature, //return the relevant data (including its db-generated id)
-//       },
-//     });
-//   } else {
-//     res.status(400).json({
-//       status: "fail",
-//       data: {
-//         name: "A string value for name is required in your JSON body",
-//       },
-//     });
-//   }
-// });
 
-// //update a signature.
-// app.put("/signatures/:id", async (req, res) => {
-//   //  :id refers to a route parameter, which will be made available in req.params.id
-//   const { name, message } = req.body;
-//   const id = parseInt(req.params.id);
-//   if (typeof name === "string" && name !== "") {
-
-//     const result: any = await client.query('UPDATE signatures SET message = $1,signature = $2 WHERE id = $3 RETURNING *;', [message, name, id]); //FIXME-TASK: update the signature with given id in the DB.
-
-//     if (result.rowCount === 1) {
-//       const updatedSignature = result.rows[0];
-//       res.status(200).json({
-//         status: "success",
-//         data: {
-//           signature: updatedSignature,
-//         },
-//       });
-//     } else {
-//       res.status(404).json({
-//         status: "fail",
-//         data: {
-//           id: "Could not find a signature with that id identifier",
-//         },
-//       });
-
-//     }
-//   } else {
-//     res.status(400).json({
-//       status: "fail",
-//       data: {
-//         name: "A string value for name is required in your JSON body",
-//       },
-//     });
-//   }
-// });
-
-// app.delete("/signatures/:id", async (req, res) => {
-//   const id = parseInt(req.params.id); // params are string type
-
-//   const queryResult: any = await client.query('DELETE from signatures WHERE id = $1;', [id]); ////FIXME-TASK: delete the row with given id from the db  
-//   const didRemove = queryResult.rowCount === 1;
-
-//   if (didRemove) {
-//     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE#responses
-//     // we've gone for '200 response with JSON body' to respond to a DELETE
-//     //  but 204 with no response body is another alternative:
-//     //  res.status(204).send() to send with status 204 and no JSON body
-//     res.status(200).json({
-//       status: "success",
-//     });
-//   } else {
-//     res.status(404).json({
-//       status: "fail",
-//       data: {
-//         id: "Could not find a signature with that id identifier",
-//       },
-//     });
-//   }
-// });
 
 export default app;
