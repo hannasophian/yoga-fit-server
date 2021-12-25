4e22a7ce562b7b315555f07793c027097694ef07
Hanna <sophianhanna@gmail.com>
2021-12-25 23:16:02 +0000

    Got it to work now?

--- src/utils/getAllVideos.ts
+++ src/utils/getAllVideos.ts
@@ -1,17 +1,30 @@
 import { Client } from "pg";
+
+import { config } from "dotenv";
+// const client = new Client({ database: "yogadb" });
+// client.connect();
+config();
+
 const herokuSSLSetting = {
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 };
 const localSetting = {
-  database: "yogadb"
+  // database: "yogadb"
+  connectionString: process.env.DATABASE_URL,
+  ssl: false,
 }
-const dbConfig = process.env.LOCAL ? herokuSSLSetting: localSetting;
+const dbConfig = process.env.LOCAL ?  localSetting: herokuSSLSetting;
 // console.log(process.env.LOCAL)
 // // console.log(sslSetting)
 // // const dbConfig = ;
 const client = new Client(dbConfig);
+async function clientConnect() {
+  await client.connect();
+}
+clientConnect();
 
+// console.log(process.env.DATABASE_URL)
 export default async function getAllVideos() {
   const videos = await client.query("SELECT * FROM vids;");
   return videos;
